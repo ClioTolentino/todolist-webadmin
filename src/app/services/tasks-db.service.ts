5975ce642d30cf5105c668cc2df77c3120877444
ClyoTolentino <clyotolentino@gmail.com>
2018-02-07 11:41:22 -0600

    FIX: Invalid server URL

--- src/app/services/tasks-db.service.ts
+++ src/app/services/tasks-db.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Task } from '../models/task';
 import { Attachment } from '../models/attachment';
-import { environment } from '../../environments/environment.prod';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class TasksDbService {
